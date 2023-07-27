6ef0c88decfff03da6954185f3c78b7a0893520d
rama <ramiirv26gmail.com>
2023-07-27 19:04:40 -0300

    before css modules changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,18 @@ import style from './Card.css';
 
 export default function Card(props) {
   return (
-    <div className='card'>
+    <div className='card grow'>
       <img src={props.image} alt='' />
-      <button onClick={props.onClose}>X</button>
-      <h2>{props.name}</h2>
+      <h2 className='name'>{props.name}</h2>
       <h2>{props.status}</h2>
       <h2>{props.species}</h2>
       <h2>{props.gender}</h2>
       <h2>{props.origin.name}</h2>
+      <div className='closeDiv'>
+        <button className='close' onClick={props.onClose}>
+          Close
+        </button>
+      </div>
     </div>
   );
 }
